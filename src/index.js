fc7e40d29ed77e98c08940d2c65743c1120ade48
Dan Balaban <dbalaban@lampix.co>
2018-11-19 11:47:05 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-import lampix from '@lampix/core';
+import lampixDOM from '@lampix/dom';
 
-lampix.getApps().then(console.log);
+import './styles.css';
+
+let counter = 0;
+
+const counterElement = document.getElementsByClassName('counter')[0];
+counterElement.textContent = 'Loading...';
+
+const increaseCount = () => {
+  counter++;
+};
+
+const updateCounterElement = () => {
+  counterElement.textContent = counter;
+};
+
+const initialize = async () => {
+  const buttonOptions = {
+    label: 'Increase count',
+    labelPosition: 'top',
+    scaleFactor: 1.2,
+    animationDuration: 250
+  };
+
+  const callback = () => {
+    increaseCount();
+    updateCounterElement();
+  };
+
+  await lampixDOM.buttons.generate(
+    window.innerWidth / 2,
+    window.innerHeight - 200,
+    callback,
+    buttonOptions
+  );
+
+  updateCounterElement();
+};
+
+initialize();
